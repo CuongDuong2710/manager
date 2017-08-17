17ccb47d36ee34b11a2871be439a6b45096cd220
CuongDuong2710 <cuongduongngocquoc@gmail.com>
2017-08-17 16:10:12 +0700

    create datasource employees list

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { View, Text } from 'react-native';
+import { ListView, View, Text } from 'react-native';
 import { employeeFetch } from '../actions';
 
 class EmployeeList extends Component {
     componentWillMount() {
         this.props.employeeFetch();
+
+        this.createDataSource(this.props);
+    }
+
+    // 'componentWillReceivedProps()' will be called whenever we are about to receive a new set of props to rerun for the component with.
+    componentWillReceivedProps(nextProps) {
+        // nextProps are the next set of props that this component
+        // will be rendered with
+        // this.props is still the old set of props
+        this.createDataSource(nextProps);
+    }
+
+    createDataSource({ employess }) {
+        const ds = new ListView.DataSource({
+                    rowHasChanged: (r1, r2) => r1 !== r2
+                 });
+
+        this.dataSource = ds.cloneWithRows(this.props.employees);
     }
 
     render() {
