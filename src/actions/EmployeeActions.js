55670995b5a474744dbab43c1b61d7d7754423bf
CuongDuong2710 <cuongduongngocquoc@gmail.com>
2017-08-17 12:26:14 +0700

    fetch data employee

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -2,7 +2,8 @@ import firebase from 'firebase';
 import { Actions } from 'react-native-router-flux';
 import {
     EMPLOYEE_UPDATE,
-    EMPLOYEE_CREATE
+    EMPLOYEE_CREATE,
+    EMPLOYEE_FETCH_SUCCESS
 } from './types';
 
 export const employeeUpdate = ({ prop, value }) => {
@@ -25,4 +26,17 @@ export const employeeCreate = ({ name, phone, shift }) => {
             });
         }
     };
+};
+
+export const employeeFetch = () => {
+    const { currentUser } = firebase.auth();
+
+    return (dispatch) => {
+        firebase.database().ref(`/users/${currentUser.uid}/employees`)
+            .on('value', snapshot => {
+                dispatch({ type: EMPLOYEE_FETCH_SUCCESS, payload: snapshot.val() });
+            });
+    }
+
+
 };
