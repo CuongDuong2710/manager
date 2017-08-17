ee5d04c5c067723afc3c7b15b012ba4769eede6c
CuongDuong2710 <cuongduongngocquoc@gmail.com>
2017-08-17 17:39:04 +0700

    add library lodash and transforming objects to array

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { ListView, View, Text } from 'react-native';
@@ -7,7 +8,7 @@ class EmployeeList extends Component {
     componentWillMount() {
         this.props.employeeFetch();
 
-        this.createDataSource(this.props);
+//        this.createDataSource(this.props);
     }
 
     // 'componentWillReceivedProps()' will be called whenever we are about to receive a new set of props to rerun for the component with.
@@ -18,7 +19,7 @@ class EmployeeList extends Component {
         this.createDataSource(nextProps);
     }
 
-    createDataSource({ employess }) {
+    createDataSource({ employees }) {
         const ds = new ListView.DataSource({
                     rowHasChanged: (r1, r2) => r1 !== r2
                  });
@@ -27,6 +28,7 @@ class EmployeeList extends Component {
     }
 
     render() {
+        console.log(this.props);
         return (
             <View>
                 <Text>Employee List</Text>
@@ -39,4 +41,14 @@ class EmployeeList extends Component {
     }
 }
 
-export default connect(null, { employeeFetch }) (EmployeeList);
+// Last step putting them into an array, map executes the federal function on every record
+// and then takes all of the returned objects and stuffs them into an array
+const mapStateToProps = state => {
+    const employees = _.map(state.employees, (val, uid) => {
+        return { ...val, uid }; // { shift: 'Monday', name: 's', id: '1j2ksj'}
+    });
+
+    return { employees };
+};
+
+export default connect(mapStateToProps, { employeeFetch }) (EmployeeList);
